f6c1567e8eaa79c70309f806f3ec8087f02e2bfd
Heyff12 <645068564@qq.com>
2019-12-07 16:17:47 +0800

    fix style

--- appRes.js
+++ appRes.js
@@ -15,16 +15,6 @@ app.post('/case', (req, res) => {
 });
 
 
-//proxy跨域演示接口---------------------------------------------------------------------------------------------------------
-app.get('/proxy1', (req, res) => {
-    res.send("Hello world from Proxy  :)")
-});
-app.get("/proxy2", function (req, res) {
-    res.send("I am here from 4001 ¬_¬");
-})
-
-
-
 //跨域cors-------被跨域方处理--------------------------------------------------------------------------------------------------
 app.get('/cros', (req, res) => {
     res.send("Hello world from http://localhost:4001/cros  CROS--get."); // 空格部分为表情，可能在编辑器不会显示
@@ -52,21 +42,27 @@ app.post('/jsonp', function (req, res) {
 });
 
 
-
-//跨域websocket---------------------------------------------------------------------------------------------------------
-var server = require('http').createServer();
-var io = require('socket.io')(server);
-io.on('connection', function (client) {
-    client.emit('data', 'Hello WebSocket from 4001.');
+//proxy跨域演示接口---------------------------------------------------------------------------------------------------------
+app.get('/proxy1', (req, res) => {
+    res.send("Hello world from Proxy  :)")
 });
-
+app.get("/proxy2", function (req, res) {
+    res.send("I am here from 4001 ¬_¬");
+})
 
 
 app.listen(requestPort, function() {
     console.log('Requester is listening on port ' + requestPort); // 在dos窗口会执行这个回调函数
 });
 
-//跨域websocket
+
+//跨域websocket---------------------------------------------------------------------------------------------------------
+// var server = require('http').createServer();
+// var io = require('socket.io')(server);
+// io.on('connection', function (client) {
+//     client.emit('data', 'Hello WebSocket from 4001.');
+// });
+
 // server.listen(requestPort, function() {
 //     console.log('Requester is listening on port ' + requestPort); // 在dos窗口会执行这个回调函数
 // });
